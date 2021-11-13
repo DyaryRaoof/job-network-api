2e71d96e612a9441348eeb2bc784543e1c364a34
Dyary Raoof Bayz <dyary2020@gmail.com>
2021-11-14 00:50:58 +0300

    Remove comments from index.js

--- startup/routes.js
+++ startup/routes.js
@@ -1,11 +1,8 @@
 const express = require('express');
 
 const user = require('../routes/users');
-// const contactModel = require('../routes/contacts')
 
 module.exports = function (app) {
   app.use(express.json());
   app.use('/api/user', user);
-  // app.use('/api/contact-model', contactModel)
-  // app.use('/api/notification', notification)
 };
